6c399311c1f46cfb06a6915f78c0f23f76bf1fab
nazgul.kadinova <nazgul.kadinova@gmail.com>
2024-03-30 11:46:42 -0500

    Added workflow file

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-ecr-nazgul"
+    #dynamodb_table = "state_lock"
+    key            = "tf-ecr"
+    region         = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name                 = "my-ecr-repo"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
