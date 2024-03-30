2c65258b93795aa5acb93d8164d33901cdf385a0
nazgul.kadinova <nazgul.kadinova@gmail.com>
2024-03-30 12:29:04 -0500

    corrected main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-ecr-nazgul"
+    bucket = "tf-state-ecr-nazgul"
     #dynamodb_table = "state_lock"
     #key            = "tf-ecr"
-    region         = "us-east-1"
+    region = "us-east-1"
   }
 }
 
@@ -14,6 +14,7 @@ provider "aws" {
 resource "aws_ecr_repository" "my_ecr_repo" {
   name                 = "my-ecr-repo"
   image_tag_mutability = "IMMUTABLE"
+  
   image_scanning_configuration {
     scan_on_push = true
   }
